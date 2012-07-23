afd967a10cc489dbe32d3eb9f15a69bf640b7786
Ross Delinger <rossdylan@csh.rit.edu>
2012-07-23 12:09:16 -0400

    added some basic config parsing and setting up of twisted endpoints (including ssl endpoints)

--- hxirc/main.py
+++ hxirc/main.py
@@ -52,3 +52,26 @@ class IRCFactory(Factory):
 
 def main(self):
     config = yaml.load(file("/etc/hxircd.conf").read())
+    endpoints = []
+    listeners = config['listen']
+    if 'ssl' in listeners:
+        from twisted.internet.endpoints import SSL4ServerEndpoint
+        from twisted.internet import ssl
+        ssl_endpoint = SSL4ServerEndpoint(
+                reactor,
+                listeners['ssl']['port'],
+                ssl.DefaultOpenSSLContextFactory(
+                    listeners['ssl']['key'],
+                    listeners['ssl']['cert']),
+                interface=config['ssl']['addr']
+                )
+        ssl_endpoint.listen(IRCFactory(config))
+        endpoints.append(ssl_endpoint)
+
+    if 'addr' in listeners and 'port' in listeners:
+        tcp_endpoint = TCP4ServerEndpoint(
+                reactor,
+                listeners['port'],
+                interface=listeners['addr'])
+        tcp_endpoint.listen(IRCFactory(config))
+        endpoints.append(tcp_endpoint)
