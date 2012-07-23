6a8090b063f49d4bcf3629f1c5eb04756044fcc4
Ross Delinger <rossdylan@csh.rit.edu>
2012-07-23 12:34:12 -0400

    Added logging stuff to main.py

--- hxirc/main.py
+++ hxirc/main.py
@@ -1,8 +1,23 @@
 import config
 import yaml
 from twisted.internet import reactor
+import logging
 
 
 def main(self):
     config_dict = yaml.load(file("/etc/hxircd.conf").read())
+
+    log_levels = dict(
+            debug=logging.DEBUG,
+            info=logging.INFO,
+            warning=logging.WARNING,
+            error=logging.ERROR,
+            critical=logging.CRITICAL)
+
+    logging.basicConfig(
+            filename=config_dict['logging']['file'],
+            format='%(asctime)s %(message)s',
+            level=log_levels[config_dict['logging']['level']])
+
     endpoints = config.parse_endpoints(config_dict, reactor)
+    reactor.run()
